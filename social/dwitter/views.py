b176c0c9cd41913a37b7920839eb1e5ddb3181bf
duhajarrar <duha.jarrar@gmail.com>
2022-11-21 02:01:58 +0200

    update

--- social/dwitter/views.py
+++ social/dwitter/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Profile
+from .forms import DweetForm 
 def dashboard(request):
-    return render(request, "base.html")
+    if request.method == "POST":
+        form = DweetForm(request.POST)
+        if form.is_valid():
+            dweet = form.save(commit=False)
+            dweet.user = request.user
+            dweet.save()
+    form = DweetForm()
+    return render(request, "dwitter/dashboard.html", {"form": form})
 
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
